df9f42c4be15a1008754b08e865e7e87e50ec42d
Eren <77127824+erenster2000@users.noreply.github.com>
2022-04-11 22:30:50 +0300

    static demo v1.2
    chest collider denemesi

---
+++ imergeProject/Assets/Scripts/ChestScripts/CollectObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectObject : MonoBehaviour
+{
+    private Object thisObject;
+
+    private void Awake()
+    {
+        thisObject = GetComponent<Object>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerCollider"));
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
